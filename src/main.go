1e7794708ac022d61ad7e86909d161338a7e98f9
Jongho Jeon <hojong.jjh@gmail.com>
2022-08-27 22:22:24 +0900

    temp

--- src/main.go
+++ src/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main()  {
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+var topic = "my-topic"
+var partition = 0
+var kafkaUrl = "kafka:9092"
+
+func main() {
 	println("hello world")
+	produceMessage()
+}
+
+func produceMessage() {
+	conn, err := kafka.DialLeader(context.Background(), "tcp", kafkaUrl, topic, partition)
+	if err != nil {
+		log.Fatal("failed to dial leader:", err)
+	}
+
+	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	_, err = conn.WriteMessages(
+		kafka.Message{Value: []byte("one!")},
+		kafka.Message{Value: []byte("two!")},
+		kafka.Message{Value: []byte("three!")},
+	)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+	if err := conn.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
 }
